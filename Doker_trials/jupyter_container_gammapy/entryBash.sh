f1a9f3fe08a7bcb3170c7a6d847f85d5c0c3c972
Luca Tosti <luca.tosti92@gmail.com>
2019-01-15 14:17:51 +0100

    sembra funzionare gammapy

--- Doker_trials/jupyter_container_gammapy/entryBash.sh
+++ Doker_trials/jupyter_container_gammapy/entryBash.sh
@@ -5,18 +5,16 @@
 export GAMMALIB=/usr/local/gamma
 source $GAMMALIB/bin/gammalib-init.sh
 
-######### CTOOLS #######
-
-export CTOOLS=/usr/local/gamma
-source $CTOOLS/bin/ctools-init.sh
-
 ######## Setting new bash shell for dummy oeprations ########
 
 #export CTADATA=/Users/lucatosti/Desktop/GEMINGA/1dc 
 export CTADATA=/CTA_DATA
 export CALDB=$CTADATA/caldb
 
-#export GAMMAPY_DATA=/userhome/DarkMatter_Tools
+export GAMMAPY_DATA=/home/jovyan/gammapy-extra/datasets
+export GAMMAPY_EXTRA=/home/jovyan/gammapy-extra
+export GAMMAPY_CAT=/home/jovyan/gamma-cat
+export GAMMA_CAT=/home/jovyan/gamma-cat
 
 
 bash
